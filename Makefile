61ebc0b9d8b1d3c774a1f6b995a34a0b3d4b2e33
Davide Cardillo <cardillo.davide85@gmail.com>
2022-08-18 23:04:57 +0200

    [TOOL] Add main Makefile directives able to manage projects
    Makefile -> user I/F, used to perform all operations on one specific project.
    lib.mk -> Makefile used by each project. Allows to perform all needed
    operation on it.

---
+++ Makefile
@@ -0,0 +1,34 @@
+
+
+.PHONY: list_prj cleanall
+
+PROJECT_LIST= \
+parallel2serial \
+
+PROJECT := ${project}
+
+.PHONY: clean compile analysis_sources analysis_testbenches \
+elaborate_testbench list run \
+list_prj cleanall 
+
+
+list_prj:
+	@echo "List of all projects:"
+	@for i in $(PROJECT_LIST); do \
+		echo " - $${i}"; \
+	done
+
+
+cleanall:
+	@for i in $(PROJECT_LIST); do \
+		make -C $${i} -f Makefile clean ; \
+	done
+
+
+list compile run view clean:
+ifeq ($(findstring $(PROJECT),$(PROJECT_LIST)),)
+	@echo "ERROR: project $(PROJECT) does not exist"
+else
+	@echo "*** Task $@ on project $(PROJECT) ***"
+	@make -C $(PROJECT) -f Makefile $@
+endif
---
+++ lib.mk
@@ -0,0 +1,96 @@
+VHDLEX = vhd
+
+SOURCEDIR := source
+TESTDIR := testbench
+SIMDIR := simulation
+
+#ARGUMENTS
+SIMULATION := ${simulation}
+
+#GHDL CONFIG
+GHDL := ghdl-llvm
+GHDL_FLAGS :=
+GHDL_OPTS := --std=08 --work=parallel2serial --workdir=simulation
+
+#SIMULATION OPTIONS
+WAVEFORM_VIEWER := gtkwave
+GHDL_SIM_POTS = --vcd=simulation/$(SIMULATION).vcd $(shell cat simulation/sim_ops | \
+grep $(SIMULATION) | \
+awk -F ':' '{print $$2}' )
+
+
+#TARGET FILE
+SOURCE_FILES := $(wildcard $(SOURCEDIR)/*.$(VHDLEX))
+OBJ_SOURCE := $(patsubst $(SOURCEDIR)/%.$(VHDLEX), $(SIMDIR)/%.o, $(SOURCE_FILES))
+
+TEST_FILES := $(wildcard $(TESTDIR)/*.$(VHDLEX))
+OBJ_TEST := $(patsubst $(TESTDIR)/%.$(VHDLEX), $(SIMDIR)/%.o, $(TEST_FILES))
+EXEC_TEST := $(OBJ_TEST:%.o=%)
+
+
+.PHONY: clean compile analysis_sources analysis_testbenches \
+elaborate_testbench list run
+
+$(SIMDIR)/%.o: $(SOURCEDIR)/%.$(VHDLEX)
+	@echo "Analyze of $@"
+	@$(GHDL) -a $(GHDL_FLAGS) $(GHDL_OPTS) $<
+
+analysis_sources: $(OBJ_SOURCE)
+
+
+$(SIMDIR)/%.o: $(TESTDIR)/%.$(VHDLEX)
+	@echo "Analyze of $@"
+	@$(GHDL) -a $(GHDL_FLAGS) $(GHDL_OPTS) $<
+
+analysis_testbenches: $(OBJ_TEST)
+
+
+$(SIMDIR)/%: $(SIMDIR)/%.o
+	@echo "Elaboration of $@"
+	@$(GHDL) -e $(GHDL_FLAGS) $(GHDL_OPTS) -o $@ $(notdir $@)
+
+elaborate_testbench: $(EXEC_TEST)
+
+
+compile: analysis_sources analysis_testbenches elaborate_testbench
+	@echo "compilation"
+
+
+list: 
+	@echo "******************************"
+	@echo "List of executable simulation:"
+	@for i in $(EXEC_TEST); do \
+		echo " - $${i##*/}"; \
+	done
+	@echo "******************************"
+
+
+run: compile
+ifeq ($(SIMULATION),)
+	@echo "No simulation speficied. Nothing to do!!!"
+else
+ifeq ($(findstring $(SIMULATION),$(EXEC_TEST)),)
+	@echo "Simulation $(SIMULATION) does not exist!!!"
+else
+ifeq ($(GHDL),ghdl-llvm)
+	@./simulation/$(SIMULATION) $(GHDL_SIM_POTS)
+else
+	@$(GHDL) -r $(GHDL_FLAGS) $(GHDL_OPTS) -o simulation/$(SIMULATION) $(GHDL_SIM_POTS)
+endif
+endif
+endif
+
+view: run
+ifneq ("$(wildcard simulation/$(SIMULATION).gtkw)","")
+	@echo "gtkw file for this vcd simulation, it will be used."
+	@$(WAVEFORM_VIEWER) simulation/$(SIMULATION).gtkw
+else
+	@echo "No gtkw file found for this vcd simulation."
+	@$(WAVEFORM_VIEWER) simulation/$(SIMULATION).vcd
+endif
+
+
+clean:
+	@${GHDL} --remove ${GHDL_FLAGS} ${GHDL_OPTS}
+	@${GHDL} --clean ${GHDL_FLAGS} ${GHDL_OPTS}
+	@rm $(SIMDIR)/*.o $(EXEC_TEST) $(SIMDIR)/*.vcd
